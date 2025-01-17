13de4a6887057977d4fe94c7e97153cd2a5fe0a3
Mark Ross <mark.ross@atos.net>
2025-01-17 13:32:17 +0000

    fix typo

--- deepracer-console-new-copy-to-car.sh
+++ deepracer-console-new-copy-to-car.sh
@@ -2,4 +2,4 @@
 
 sudo cp /opt/aws/deepracer/lib/device_console /opt/aws/deepracer/lib/device_console_old
 unzip deepracer-console-new.zip -d /temp_website
-sudo cp temp_website/* /opt/aws/deepracer/lib/device_console/templates/ r
+sudo cp temp_website/* /opt/aws/deepracer/lib/device_console/templates/ -r
