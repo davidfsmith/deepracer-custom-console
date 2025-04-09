c353ec4f0e00d91dfc2a9efd32d396fd005d10e3
Mark Ross <mark.ross@atos.net>
2025-04-09 08:46:52 +0100

    moved battery to use api helper

--- website/src/common/helpers/api-helper.ts
+++ website/src/common/helpers/api-helper.ts
@@ -4,7 +4,7 @@ export abstract class ApiHelper {
   static async get<T>(path: string): Promise<T | null> {
     try {
       const response: AxiosResponse<T> = await axios.get("/api/" + path, {
-        timeout: 10000 // 30 seconds timeout
+        timeout: 10000 // 10 seconds timeout
       });
       return response.data;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -35,7 +35,7 @@ export abstract class ApiHelper {
   static async post<T>(path: string, data: unknown): Promise<T | null> {
     try {
       const response: AxiosResponse<T> = await axios.post("/api/" + path, data, {
-        timeout: 10000 // 30 seconds timeout
+        timeout: 10000 // 10 seconds timeout
       });
       return response.data;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
